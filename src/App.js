7b220f1921f29ac61a8c64d4ce23f32a0aac745f
cedrou <cedrou@gmail.com>
2018-01-03 16:22:10 +0100

    Add log

--- src/App.js
+++ src/App.js
@@ -15,10 +15,13 @@ class App extends Component {
         list: vocab,
         current: ["",""],
         input: "",
-        log: [],
+        log: {},
 
         error: 0,
-        success: 0
+        success: 0,
+
+        isSuccess: false,
+        isError: false,
     };
 
 
@@ -31,15 +34,21 @@ class App extends Component {
                 let {success, log} = this.state;
 
                 success += 1;
-                log.push(`[Correct] ${this.state.current[0]} -> ${this.state.input}`);
-                this.setState({success: success, log: log});
+                log[this.state.current[0]].trials.push(this.state.input);
+                log[this.state.current[0]].success = true;
+                
+                this.setState({success: success, log: log, isSuccess: true, isError: false});
                 this.next();
+                setTimeout(() => this.setState({isSuccess: false}), 2000);
             }
             else {
                 let {error, log} = this.state;
+
                 error += 1;
-                log.push(`[Erreur ] ${this.state.current[0]} -> ${this.state.input}`);
-                this.setState({error: error, log: log});
+                log[this.state.current[0]].trials.push(this.state.input);
+
+                this.setState({error: error, log: log, isError: true, isSuccess: false});
+                setTimeout(() => this.setState({isError: false}), 2000);
             }
         }
         else if (value === "SKIP") {
@@ -58,12 +67,19 @@ class App extends Component {
         state.list.push(state.current);
         state.current = state.list.shift();
         state.input = "";
+ 
+        state.log[state.current[0]] = state.log[state.current[0]] || { trials:[], success: false}
+
         this.setState(state); 
     }
+
     next () {
         let state = this.state;
         state.current = state.list.shift();
         state.input = "";
+
+        state.log[state.current[0]] = state.log[state.current[0]] || { trials:[], success: false}
+
         this.setState(state); 
     }
 
@@ -71,15 +87,52 @@ class App extends Component {
         this.next();
     }
 
+    getAnswerClass() {
+        let className = "App-answer";
+        if (this.state.isSuccess) className += " answer-success";
+        if (this.state.isError) className += " answer-error";
+        return className;
+    }
+
+    renderLog() {
+        return (
+            <div className="App-log">
+                <table>
+                    <thead>
+                        <th className="index" >#</th>
+                        <th className="word" >Mot</th>
+                        <th className="trials" >Essais</th>
+                    </thead>
+                    {Object.keys(this.state.log).map( (key,i) => {
+                        let item = this.state.log[key];
+
+                        return (
+                            <tbody className={item.success ? "log-success" : "log-error"}>
+                            <tr>
+                                <td>{i+1}</td>
+                                <td>{key}</td>
+                                <td><ul className="horizontal-stack">
+                                {item.trials.map ( (trial,index) => {
+                                    let className = (item.success && index === item.trials.length - 1) ? "bolded" : "striked";
+                                    return <li className={className}>{trial}</li>
+                                })}
+                                </ul></td>
+                            </tr></tbody>
+                        );
+                    })}
+                </table>
+            </div>
+        )
+    }
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Test de vocabulaire</h1>
+                    <h1 className="App-title">Test de vocabulaire franco-russe</h1>
                 </header>
 
                 <div className="App-quizz">
-                    <table ><tbody>
+                    <table><tbody>
                         <tr>
                             <td>
                             <img src={frflag} className="App-flag" alt="frflag" />
@@ -87,7 +140,7 @@ class App extends Component {
                             </td>
                             <td>
                                 <img src={ruflag} className="App-flag" alt="ruflag"/>
-                                <div className="App-answer">{this.state.input} </div>
+                                <div className={this.getAnswerClass()}>{this.state.input}</div>
                             </td>
                         </tr>
                     </tbody></table>
@@ -95,6 +148,7 @@ class App extends Component {
  
                 <Keyboard onClick={this.handleKeyboardClick} />
 
+                {this.renderLog()}
             </div>
         );
     }
