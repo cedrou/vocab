61f9bb6353c61d5319272a89c9a6b8d4bbac5741
cedrou <cedrou@gmail.com>
2018-01-04 11:38:45 +0100

    Changed keyboard layout mechanism

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -9,11 +9,34 @@ const ShiftIcon = ({viewBox = '0 0 32 32', width = 24, height = 24, fill}) =>
 	</svg>
 ;
 
-const BackspaceIcon = ({viewBox = '0 0 24 24', width = 24, height = 24, fill,}) =>
+const BackspaceIcon = ({viewBox = '0 0 24 24', width = 24, height = 24, fill}) =>
 	<svg {...{width, height, fill, viewBox}}>
 		<path fill="currentColor" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12 19 15.59z" />
 	</svg>
 ;
+
+const Key_Backspace = {id: 'BACKSPACE', value:<BackspaceIcon/>, class:"keyboard-backspace-button"}
+const Key_Shift = {id: 'SHIFT', value:<ShiftIcon/>, class:"keyboard-shift-button"}
+const Key_Skip = {id: 'SKIP', value:"Passer", class:"keyboard-skip-button"}
+const Key_Submit = {id: 'SUBMIT', value:"Envoyer", class:"keyboard-submit-button"}
+const Key_Space = {id: " ", value:" ", class:"keyboard-space-button"}
+
+const Layout_Cyrillic = [
+    [
+        ['ë', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\\', Key_Backspace],
+        ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
+        ['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'],
+        [Key_Shift, 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', Key_Shift],
+        [Key_Skip, Key_Space, Key_Submit]
+    ],
+    [
+        ['Ë', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', '/', Key_Backspace],
+        ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ'],
+        ['Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э'],
+        [Key_Shift, 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', Key_Shift],
+        [Key_Skip, Key_Space, Key_Submit]
+    ],
+];
 
 class Keyboard extends React.Component {
 
@@ -21,104 +44,40 @@ class Keyboard extends React.Component {
         uppercase: false
     }
 
-    Layout_Cyrillic = [
-		['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
-		['ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э'],
-		['я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'],
-    ]
-    
-    handleAlphanumClick = (key) => {
-        this.props.onClick(key);
-        this.setState({uppercase: false});
-    }
-
-    handleShiftClick = () => this.setState({uppercase: !this.state.uppercase});
-    
-    handleBackspaceClick = () => this.props.onClick("BACKSPACE");
-    handleSkipClick = () => this.props.onClick("SKIP");
-    handleSubmitClick = () => this.props.onClick("SUBMIT");
- 
-
-    renderFirstRow() {
-        return (
-            <div className="keyboard-row">
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(v =>
-                    <KeyboardButton 
-                        key={v} value={v} 
-                        onClick={this.handleAlphanumClick}
-                        className="keyboard-number-button"
-                    />
-                )}
-                <KeyboardButton
-                    value={<BackspaceIcon />}
-                    className="keyboard-backspace-button"
-                    onClick={this.handleBackspaceClick}
-                />
-            </div>
-        )
+    handleKeyClick = (key) => {
+        if (key === "SHIFT") {
+            this.setState({uppercase: !this.state.uppercase});
+        }
+        else {
+            this.props.onClick(key);
+            this.setState({uppercase: false});
+        }
     }
 
     renderLayout() {
-        let layout = this.Layout_Cyrillic;
-        if (this.state.uppercase) {
-            layout = layout.map(row => row.map(key => key.toUpperCase()));
-        }
+        let layout = Layout_Cyrillic[this.state.uppercase ? 1 : 0];
 
         return layout.map ( (row,i) =>
             <div className="keyboard-row" key={`row-${i}`}>
-                {i === layout.length - 1 &&
-                    <KeyboardButton
-                        value={<ShiftIcon />}
-                        className="keyboard-shift-button"
-                        onClick={this.handleShiftClick}
-                    />
-                }
-                {row.map(button =>
-                    <KeyboardButton
-                        key={button} value={button}
-                        onClick={this.handleAlphanumClick}
-                    />
-                )}
-                {i === layout.length - 1 &&
-                    <KeyboardButton
-                        value={<ShiftIcon />}
-                        className="keyboard-shift-button"
-                        onClick={this.handleShiftClick}
-                    />
-                }
-            </div>
-        )
-    }
 
-    renderLastRow() {
-        return (
-            <div className="keyboard-row">
-                <KeyboardButton
-                      value="Passer"
-                      className="keyboard-skip-button"
-                      onClick={this.handleSkipClick}
-                    />
-                <KeyboardButton
-						value={' '}
-						className="keyboard-space-button"
-						onClick={this.handleAlphanumClick}
-					/>
-                <KeyboardButton
-                      value="Envoyer"
-                      className="keyboard-submit-button"
-                      onClick={this.handleSubmitClick}
-                    />
+                {row.map(k => {
+                    
+                    if (typeof(k) === 'string') {
+                        k = {id: k, value: k}
+                    }
+                    
+                    return <KeyboardButton key={k.id} id={k.id} value={k.value} className={k.class} onClick={this.handleKeyClick} />
+                    
+                })}
+
             </div>
         )
-        
     }
 
     render() {
         return (
             <div className="keyboard">
-                {this.renderFirstRow()}
                 {this.renderLayout()}
-                {this.renderLastRow()}
             </div>
         );
     }
--- src/KeyboardButton.js
+++ src/KeyboardButton.js
@@ -4,7 +4,7 @@ class KeyboardButton extends React.Component {
 
     render() {
         return (
-            <button className={`keyboard-button ${this.props.className}`} onClick={() => this.props.onClick(this.props.value)}>
+            <button className={`keyboard-button ${this.props.className}`} onClick={() => this.props.onClick(this.props.id)}>
                 {this.props.value}
             </button>
 
